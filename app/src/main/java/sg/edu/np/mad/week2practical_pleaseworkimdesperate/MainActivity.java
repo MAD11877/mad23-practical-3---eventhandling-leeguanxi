74a38706c0cb3e402685a0db1a6778bc6ce0bd30
leeguanxi <130948753+leeguanxi@users.noreply.github.com>
2023-05-08 11:14:32 +0800

    Add files via upload

---
+++ app/src/main/java/sg/edu/np/mad/week2practical_pleaseworkimdesperate/MainActivity.java
@@ -0,0 +1,15 @@
+package sg.edu.np.mad.week2practical_pleaseworkimdesperate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
