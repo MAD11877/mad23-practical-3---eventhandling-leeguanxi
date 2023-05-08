3c2d90ffbbbfa69e31baea6f57230f709f15f411
Guan Xi <s10244223@connect.np.edu.sg>
2023-05-09 00:15:29 +0800

    week03 practical 9/5/23 00:14

--- app/src/main/java/sg/edu/np/mad/week2practical_pleaseworkimdesperate/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week2practical_pleaseworkimdesperate/MainActivity.java
@@ -1,8 +1,15 @@
 package sg.edu.np.mad.week2practical_pleaseworkimdesperate;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +17,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //
+    }
+    boolean followed = false;
+    public void onFollowClick(View v) {
+        Log.v("HELLO", "Button Clicked!");
+        if (followed){
+            Toast.makeText(getBaseContext(),"Unfollowed", Toast.LENGTH_SHORT).show();
+            Button button = (Button)findViewById(R.id.btnFollow);
+            button.setText("FOLLOW");
+            followed = false;
+        }
+        else {
+            Toast.makeText(getBaseContext(),"Followed", Toast.LENGTH_SHORT).show();
+            Button button = (Button)findViewById(R.id.btnFollow);
+            button.setText("UNFOLLOW");
+            followed = true;
+        }
+
+    }
+
+    public void onMessageClick(View v) {
+        // MainActivity to MessageGroup
+        Intent intent = new Intent(MainActivity.this, MessageGroup.class);
+        startActivity(intent);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        // receive data from ListActivity
+        Intent intent = getIntent();
+        int randomInt = intent.getIntExtra("random_integer", 0);
+        TextView textview = findViewById(R.id.textView);
+        textview.setText("MAD " + randomInt);
     }
 }
